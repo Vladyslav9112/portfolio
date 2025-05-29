71b4843a3a75ef8816551155f4d08579600d9d50
Vladyslav Kryklyvets <95692328+Vladyslav9112@users.noreply.github.com>
2025-05-29 22:35:59 +0300

    I translated the entire project to next and created (Content Wrapper & Header)

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,25 @@
+import ContentWrapper from "./global/ContentWrapper";
+
+export default function Header() {
+  return (
+    <section className="w-[1280px] bg-custom-gradient mt-10 h-20 flex items-center justify-between text-white px-6">
+      <div className="bg-black shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-[100px] w-[257px] h-[45px] flex justify-center items-center">
+        <a href="#">VIEW RESUME</a>
+      </div>
+      <nav className="gap-15">
+        <a href="#" className="mr-15">
+          Home
+        </a>
+        <a href="#" className="mr-15">
+          About
+        </a>
+        <a href="#" className="mr-15">
+          Projects
+        </a>
+        <a href="#" className="mr-15">
+          Contacts
+        </a>
+      </nav>
+    </section>
+  );
+}
---
+++ src/app/components/global/ContentWrapper.jsx
@@ -0,0 +1,3 @@
+export default function ContentWrapper({ children }) {
+  return <section className="mx-auto w-[1440px] px-20">{children}</section>;
+}

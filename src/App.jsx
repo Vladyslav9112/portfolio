021cb3a24c5c69b20d4b0a8f76661085764bbc4a
Vladyslav <vlad.kryklyvets9@gmail.com>
2025-05-29 14:16:17 +0300

    created portfolio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./styles/main.scss";
+import ContentWrapper from "./components/contentWrapper";
+
+function App() {
+  return (
+    <>
+      <ContentWrapper></ContentWrapper>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ContentWrapper.jsx
@@ -0,0 +1,7 @@
+export default function ContentWrapper() {
+  return (
+    <section className="wrapper">
+      <h1></h1>
+    </section>
+  );
+}

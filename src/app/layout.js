d0da54999557134c9f54ec069960684c57ca9136
Vladyslav <vlad.kryklyvets9@gmail.com>
2025-06-11 20:10:06 +0300

    Added metadata and updated the loyalty structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,59 +1,22 @@
-"use client";
-
-import { useEffect } from "react";
 import "./styles/globals.css";
 import "./styles/animation.css";
 import "./styles/font.css";
+import ClientLayout from "./ClientLoyout"; // 👈 Імпорт клієнтської оболонки
+
+export const metadata = {
+  title: "PORTFOLIO: Vlad Kryklyvets",
+  description:
+    "Personal portfolio showcasing projects, skills, and experience.",
+  icons: {
+    icon: "/icons/favicon.ico",
+  },
+};
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    const container = document.querySelector(".glows-container");
-    if (!container) return;
-
-    container.innerHTML = "";
-
-    const screenWidth = window.innerWidth;
-
-    let numGlows = 20;
-    if (screenWidth < 640) {
-      numGlows = 5;
-    } else if (screenWidth < 768) {
-      numGlows = 7 + Math.floor(Math.random() * 4);
-    } else if (screenWidth < 1024) {
-      numGlows = 10 + Math.floor(Math.random() * 6);
-    }
-
-    for (let i = 0; i < numGlows; i++) {
-      const glow = document.createElement("div");
-      glow.className = "glow";
-
-      glow.style.top = `${Math.random() * 100}vh`;
-      glow.style.left = `${Math.random() * 100}vw`;
-
-      const size = 80 + Math.random() * 40;
-      glow.style.width = `${size}px`;
-      glow.style.height = `${size}px`;
-
-      const duration = 2 + Math.random() * 3;
-      const delay = Math.random() * duration;
-
-      const moveX = (Math.random() - 0.5) * 40;
-      const moveY = (Math.random() - 0.5) * 80;
-
-      glow.style.setProperty("--move-x", `${moveX}vw`);
-      glow.style.setProperty("--move-y", `${moveY}vh`);
-      glow.style.animation = `moveGlow ${duration}s ease-in-out infinite alternate`;
-      glow.style.animationDelay = `${delay}s`;
-
-      container.appendChild(glow);
-    }
-  }, []);
-
   return (
     <html lang="en">
       <body className="bg-black text-white relative min-h-screen overflow-x-hidden">
-        <div className="glows-container fixed top-0 left-0 w-full h-full pointer-events-none -z-10"></div>
-        {children}
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );

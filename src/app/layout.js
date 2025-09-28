4cd87b86a0b30f78f8ea56e77db37a74b519e5cb
Vladyslav <vlad.kryklyvets9@gmail.com>
2025-09-28 21:17:23 +0300

    adding photos to meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import "./styles/globals.css";
 import "./styles/animation.css";
 import "./styles/font.css";
 import ClientLayout from "./ClientLoyout"; // 👈 Імпорт клієнтської оболонки
-
 export const metadata = {
   title: "PORTFOLIO: Vlad Kryklyvets",
   description:
@@ -24,6 +23,28 @@ export const metadata = {
   robots: "index, follow",
   icons: {
     icon: "/icons/favicon.ico",
+  },
+  openGraph: {
+    title: "PORTFOLIO: Vlad Kryklyvets",
+    description:
+      "Vlad Kryklyvets - Front-End Developer from Ukraine. Portfolio with projects, skills, and contact info.",
+    url: "https://portfolio-omega-gilt-20.vercel.app",
+    siteName: "Vlad Kryklyvets Portfolio",
+    images: [
+      {
+        url: "https://i.postimg.cc/3NbpH7Jm/Screenshot-2.jpg",
+        width: 1200,
+        height: 627,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "PORTFOLIO: Vlad Kryklyvets",
+    description:
+      "Vlad Kryklyvets - Front-End Developer from Ukraine. Portfolio with projects, skills, and contact info.",
+    images: ["https://i.postimg.cc/3NbpH7Jm/Screenshot-2.jpg"],
   },
 };
 
